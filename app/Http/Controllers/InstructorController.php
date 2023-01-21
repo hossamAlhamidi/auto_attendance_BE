7b45e1a90fcbe20b55ed99e3fa485d43377cf101
hossamAlhamidi <hossam-1418@hotmail.com>
2023-01-21 18:49:31 +0300

    token , instructor update , show

--- app/Http/Controllers/InstructorController.php
+++ app/Http/Controllers/InstructorController.php
@@ -7,7 +7,8 @@ use Illuminate\Http\Request;
 use Illuminate\Support\Facades\DB;
 use App\Models\Student_Section;
 use App\Models\Section;
-
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Hash;
 class InstructorController extends Controller
 {
     /**
@@ -17,7 +18,7 @@ class InstructorController extends Controller
      */
     public function index()
     {
-        return Instructor::all();
+         return Instructor::select('instructor_id', 'instructor_name', 'email','phone_number')->get();
     }
 
     /**
@@ -37,10 +38,23 @@ class InstructorController extends Controller
      * @param  int  $id
      * @return \Illuminate\Http\Response
      */
-    public function show($id)
+    public function show()
     {
-        //
+        $user = Auth::guard('sanctum')->user();
+        $instructor_id = $user->instructor_id;
+            $instructor = Instructor::Where('instructor_id',$instructor_id)->get(['instructor_id','instructor_name','email','phone_number']);
+            if(count($instructor)>0){
+                return $instructor;
+            }
+            else {
+                return response([
+                    'message'=>'No found instructor by This ID'
+                ],401);
+            }
+        
     }
+
+
 
     /**
      * Update the specified resource in storage.
@@ -49,28 +63,59 @@ class InstructorController extends Controller
      * @param  int  $id
      * @return \Illuminate\Http\Response
      */
-    public function update(Request $request, $id)
+    public function update(Request $request)
     {
-        $validated_id = htmlspecialchars($id);
+        // $validated_id = htmlspecialchars($id);
 
-        $instructor = Instructor::Where('instructor_id', $validated_id)->first();
+        // $instructor = Instructor::Where('instructor_id', $validated_id)->first();
 
-        if(!$instructor){
-            return response([
-                'message'=>'No Instructor found by This ID'
-            ],401);
-        }
+        // if(!$instructor){
+        //     return response([
+        //         'message'=>'No Instructor found by This ID'
+        //     ],401);
+        // }
 
-        $instructor->update($request->all());
+        // $instructor->update($request->all());
 
-        $response = [
-            'instructor_id' => $instructor['instructor_id'],
-            'instructor_name' => $instructor['instructor_name'],
-            'email' => $instructor['email'],
-            'phone_number' => $instructor['phone_number']
-        ];
+        // $response = [
+        //     'instructor_id' => $instructor['instructor_id'],
+        //     'instructor_name' => $instructor['instructor_name'],
+        //     'email' => $instructor['email'],
+        //     'phone_number' => $instructor['phone_number']
+        // ];
 
-        return response($response, 201);
+        // return response($response, 201);
+
+        $user = Auth::guard('sanctum')->user();
+        $instructor_id = $user->instructor_id;
+            $instructor = Instructor::findOrFail($instructor_id);
+            
+            $validatedData = $request->validate([
+                'instructor_name' => 'sometimes|string|max:50',
+                'email' => 'sometimes|string|email|max:50',
+                'phone_number' => 'sometimes|string|max:15',
+                'password' => 'sometimes|string|min:3|confirmed',
+                'old_password' => 'required_with:password|string'
+            ]);
+    
+            if(array_key_exists('password',$validatedData)){
+                // check if the old password provided is correct
+                if (!Hash::check($validatedData['old_password'], $instructor->password)) {
+                    return response(['message' => 'Old password is not correct'], 401);
+                }
+                $validatedData['password'] = bcrypt($validatedData['password']);
+            }
+            $instructor->fill($validatedData);
+            if ($instructor->isDirty()) {
+                $instructor->save();
+                return response(['message' => 'Instructor information updated successfully']);
+            }else{
+                return response(['message' => 'No changes made']);
+            }
+            // $instructor->update($validatedData);
+    
+            // return response(['message' => 'Instructor information updated successfully']);
+        
     }
 
     /**
