27f5b4d89dcecc6d6a32581b2f159fa99cdf1936
hossamAlhamidi <hossam-1418@hotmail.com>
2023-01-28 03:29:52 +0300

    dash fix

--- app/Http/Controllers/DashboardController.php
+++ app/Http/Controllers/DashboardController.php
@@ -17,23 +17,25 @@ class DashboardController extends Controller
     public function MostRegisteredCourses($number = 5)
     {
         // return StudentCourse::groupBy('course_id')->count();
-        $var = DB::table('student_courses')->select('course_id', DB::raw('count(*) as number'))->groupBy('course_id')->orderByDesc('number')->orderBy('course_id', 'asc')->simplePaginate($number);
-        $response = $var->toArray();
-        return response()->json($response['data'],200);
+        $var = DB::table('student_courses')->select('course_id', DB::raw('count(*) as number'))->groupBy('course_id')->orderByDesc('number')->orderBy('course_id', 'asc')->take($number)->get();
+        return response()->json($var,200);
     }
 
     public function MostAbsenceInSection($number = 5)
     {
-        $var = DB::table('absences')->select('section_id', DB::raw('count(*) as number'))->groupBy('section_id')->orderByDesc('number')->orderBy('section_id', 'asc')->simplePaginate($number);
-        $response = $var->toArray();
-        return response()->json($response['data'],200);
+        // $var = DB::table('absences')->select('section_id', DB::raw('count(*) as number'))->groupBy('section_id')->orderByDesc('number')->orderBy('section_id', 'asc')->simplePaginate($number);
+        // $response = $var->toArray();
+        // return response()->json($response['data'],200);
+        $var = DB::table('absences')->select('section_id', DB::raw('count(*) as number'))->groupBy('section_id')->orderByDesc('number')->orderBy('section_id', 'asc')->take($number)->get();
+   
+        return response()->json($var,200);
     }
 
     public function MostInstructorTeaching($number = 5)
     {
-        $var = DB::table('sections')->select('instructor_id', DB::raw('count(*) as number'))->groupBy('instructor_id')->orderByDesc('number')->orderBy('instructor_id', 'asc')->simplePaginate($number);
-        $response = $var->toArray();
-        return response()->json($response['data'],200);
+        $var = DB::table('sections')->select('instructor_name', DB::raw('count(*) as number'))->groupBy('instructor_name')->orderByDesc('number')->orderBy('instructor_name', 'asc')->take($number)->get();
+        return response()->json($var,200);
+        
     }
 
     public function NumberOfAbsence($number = 5)
